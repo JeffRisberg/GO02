56a1eaaafe993fb0d63759eefebba618c7f0e143
JeffRisberg <jrisberg@aol.com>
2019-06-09 12:47:35 +0200

    added examples of writeToFile and readFromFile.

--- main.go
+++ main.go
@@ -9,4 +9,12 @@ func main() {
 	hand.print()
 	fmt.Println("")
 	remainingDeck.print()
+
+	fmt.Println(hand.toString())
+
+	hand.saveToFile("hand20190609")
+
+	hand2 := newDeckFromFile("hand20190609")
+
+	hand2.print();
 }
