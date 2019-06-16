f8c300670b56be0f266a0c53dcfe8c98bd698a20
JeffRisberg <jrisberg@aol.com>
2019-06-16 18:11:49 +0200

    added example of structs

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstname string
-	lastname string
+	lastname  string
+	contactInfo
+}
+
+func (p person) display() {
+	fmt.Println(p.firstname + p.lastname)
+	fmt.Println(p.contactInfo.email)
 }
 
 func main() {
-	person1 := person{"bob", "Jones"}
-	fmt.Println(person1);
+	contactInfo1 := contactInfo{email: "bob@gmail.com"}
 
+	person1 := person{firstname: "bob", lastname: "Jones", contactInfo: contactInfo1}
+
+	fmt.Println(person1);
+	person1.display()
+	/*
 	cards := newDeck()
 	hand, remainingDeck := deal(cards, 5)
 
@@ -25,4 +40,5 @@ func main() {
 	hand2 := newDeckFromFile("hand20190609")
 
 	hand2.print()
+	*/
 }
