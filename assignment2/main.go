8ccb7a982266d260efbbbb7478b3af93eec7fcb8
JeffRisberg <jrisberg@aol.com>
2019-06-19 18:02:54 -0700

    begin development of lesson 2

--- assignment2/main.go
+++ assignment2/main.go
@@ -2,41 +2,41 @@ package main
 
 import "fmt"
 
+type square struct {
+	sideLength float64
+}
+
 type triangle struct {
 	base   float64
 	height float64
-}
-
-type square struct {
-	sideLength float64
 }
 
 type shape interface {
 	getArea() float64
 }
 
-func main() {
-	t := triangle{
-		base:   10,
-		height: 5,
-	}
-
-	s := square{
-		sideLength: 6,
-	}
-
-	fmt.Println(printArea(t))
-	fmt.Println(printArea(s))
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
 }
 
 func (t triangle) getArea() float64 {
 	return 0.5 * t.base * t.height
 }
 
-func (s square) getArea() float64 {
-	return s.sideLength * s.sideLength
-}
-
 func printArea(s shape) float64 {
 	return s.getArea()
+}
+
+func main() {
+	square1 := square{
+		sideLength: 6,
+	}
+
+	triangle1 := triangle{
+		base:   10,
+		height: 5,
+	}
+
+	fmt.Println(printArea(square1))
+	fmt.Println(printArea(triangle1))
 }
