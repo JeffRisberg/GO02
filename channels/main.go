56b15b99958979ba17e925857e85620b957bf5c6
JeffRisberg <jrisberg@aol.com>
2019-06-30 03:14:25 -0700

    example of using channels

--- channels/main.go
+++ channels/main.go
@@ -19,14 +19,19 @@ func main() {
 	for _, link := range links {
 		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "down"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "up"
 }
