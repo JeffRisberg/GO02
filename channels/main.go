fcdaff4a58ceac50def213203b1c7a69182aee68
JeffRisberg <jrisberg@aol.com>
2019-06-29 22:14:57 -0700

    added channels example

--- channels/main.go
+++ channels/main.go
@@ -21,7 +21,7 @@ func main() {
 	}
 }
 
-func checkLink(link string, c string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
